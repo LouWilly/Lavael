7e7eacd7bd78176546ea4b4acbc6ead99a9fc954
LouWilly <mllf.flor@gmail.com>
2022-03-29 10:51:03 -0300

    first commit

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    public function cadastrar(){
+        echo 'me da MB faz favor, te amo uWu S2';
+    }
+}
